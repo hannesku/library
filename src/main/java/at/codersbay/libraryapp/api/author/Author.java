30da8834e28266ef2f1d5cd2568f95ef563ec689
hannesku <hannes.kurth@gmx.netr>
2024-03-04 20:57:33 +0100

    src-folder and pom.xml

---
+++ src/main/java/at/codersbay/libraryapp/api/author/Author.java
@@ -0,0 +1,69 @@
+package at.codersbay.libraryapp.api.author;
+
+import at.codersbay.libraryapp.api.books.Book;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "TB_AUTHORS")
+public class Author {
+
+    @Id
+    @GeneratedValue(generator = "author-sequence-generator")
+    @GenericGenerator(
+            name = "author-sequence-generator",
+            strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator",
+            parameters = {
+                    @org.hibernate.annotations.Parameter(name = "sequence_name", value = "author_sequence"),
+                    @org.hibernate.annotations.Parameter(name = "initial_value", value = "1"),
+                    @org.hibernate.annotations.Parameter(name = "increment_size", value = "1")
+            }
+    )
+    private long id;
+
+    @Column()
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+
+    @JsonIgnore
+    @ManyToMany(mappedBy = "authors")
+    private Set<Book> books;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
+    }
+}
---
+++ src/main/java/at/codersbay/libraryapp/api/books/Book.java
@@ -0,0 +1,73 @@
+package at.codersbay.libraryapp.api.books;
+
+import at.codersbay.libraryapp.api.author.Author;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+
+import java.util.Set;
+
+@Entity
+@Table(name = "TB_BOOKS")
+public class Book {
+
+    @Id
+    @GeneratedValue(generator = "book-sequence-generator")
+    @GenericGenerator(
+            name = "book-sequence-generator",
+            strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator",
+            parameters = {
+                    @org.hibernate.annotations.Parameter(name = "sequence_name", value = "book_sequence"),
+                    @org.hibernate.annotations.Parameter(name = "initial_value", value = "1"),
+                    @org.hibernate.annotations.Parameter(name = "increment_size", value = "1")
+            }
+    )
+    private long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false, unique = false)
+    private String isbn;
+
+
+    @ManyToMany()
+    @JoinTable(
+            name = "TB_BOOKS_AUTHORS",
+            joinColumns = @JoinColumn(name = "book_id"),
+            inverseJoinColumns = @JoinColumn(name = "author_id"))
+    private Set<Author> authors;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Set<Author> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(Set<Author> authors) {
+        this.authors = authors;
+    }
+}
